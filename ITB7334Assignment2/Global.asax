4777f3b35ab46cd061cccee73902db77fbdd323d
hemrav07 <hemaraval7@gmail.com>
2017-06-16 17:19:17 +1200

    Add project files.

---
+++ ITB7334Assignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITB7334Assignment2.MvcApplication" Language="C#" %>
---
+++ ITB7334Assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITB7334Assignment2.Startup))]
+namespace ITB7334Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
